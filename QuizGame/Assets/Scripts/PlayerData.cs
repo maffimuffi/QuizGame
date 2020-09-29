57abc549574a60a045170af262e9e149c3f82118
maffimuffi <tukiainen.nike@gmail.com>
2020-09-29 20:08:22 +0300

    Mobile version menu stuff and save/load

---
+++ QuizGame/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+
+    public int level;
+    public int correctAnswers;
+    public int wrongAnswers;
+    public int questionsLeft;
+    public int score;
+    public int highscore;
+    public int gameState;
+
+    public float timeLeft;
+
+    public bool roundEnded;
+    public bool answering;
+    public bool playerAnswer;
+
+    public PlayerData(GameManager manager)
+    {
+        level = manager.level;
+        correctAnswers = manager.correctAnswers;
+        wrongAnswers = manager.wrongAnswers;
+        questionsLeft = manager.questionsLeft;
+        score = manager.score;
+        highscore = manager.highscore;
+        gameState = manager.gameState;
+
+        timeLeft = manager.timeLeft;
+
+        roundEnded = manager.roundEnded;
+        answering = manager.answering;
+        playerAnswer = manager.playerAnswer;
+    }
+
+}
---
+++ QuizGame/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public GameObject settingsScreen;
+    public GameObject mainmenuScreen;
+
+    public void BackButton()
+    {
+        settingsScreen.SetActive(false);
+        mainmenuScreen.SetActive(true);
+    }
+
+    // Code to change music and sound volume from sliders
+}
