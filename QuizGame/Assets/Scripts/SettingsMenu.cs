1028ec0adea344109d80c4fedcee621743e6c66a
maffimuffi <tukiainen.nike@gmail.com>
2020-10-01 20:10:09 +0300

    Mobile game ui and stuff

--- QuizGame/Assets/Scripts/SettingsMenu.cs
+++ QuizGame/Assets/Scripts/SettingsMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -8,11 +9,66 @@ public class SettingsMenu : MonoBehaviour
     public GameObject settingsScreen;
     public GameObject mainmenuScreen;
 
+    public Button musicButton;
+    public Button soundButton;
+
+    [SerializeField] private bool musicOn;
+    [SerializeField] private bool soundOn;
+
+    public void MusicButton()
+    {
+        if(musicOn)
+        {
+            musicOn = false;
+            musicButton.image.color = Color.red;
+        }
+        else if(!musicOn)
+        {
+            musicOn = true;
+            musicButton.image.color = Color.green;
+        }
+    }
+
+    public void SoundButton()
+    {
+        if(soundOn)
+        {
+            soundOn = false;
+            soundButton.image.color = Color.red;
+        }
+        else if(!soundOn)
+        {
+            soundOn = true;
+            soundButton.image.color = Color.green;
+        }
+    }
+
     public void BackButton()
     {
         settingsScreen.SetActive(false);
         mainmenuScreen.SetActive(true);
     }
 
-    // Code to change music and sound volume from sliders
+    void Start()
+    {
+        musicOn = true;
+        soundOn = true;
+
+        if(musicOn)
+        {
+            musicButton.image.color = Color.green;
+        }
+        else if(!musicOn)
+        {
+            musicButton.image.color = Color.red;
+        }
+        if (soundOn)
+        {
+            soundButton.image.color = Color.green;
+        }
+        else if (!soundOn)
+        {
+            soundButton.image.color = Color.red;
+        }
+    }
 }
