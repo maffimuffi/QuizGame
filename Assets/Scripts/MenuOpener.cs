0d1db2b101ee0c0ebdc34d16dfd665d1a713fc94
ferrylehtinen <ferry.lehtinen@metropolia.fi>
2020-10-27 17:55:07 +0200

    Simple animation which is too complicated

---
+++ Assets/Scripts/MenuOpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpener : MonoBehaviour
+{
+    public GameObject gameScreen;
+
+
+    public void OpenGameScreen()
+    {
+        if(gameScreen != null)
+        {
+            //bool isActive = gameScreen.activeSelf;
+
+            //gameScreen.SetActive(!isActive);
+
+            Animator animator = gameScreen.GetComponent<Animator>();
+            if(animator != null)
+            {
+                bool isOpen = animator.GetBool("OpenMenu");
+                animator.SetBool("OpenMenu", !isOpen);
+            }
+        }
+    }
+}
