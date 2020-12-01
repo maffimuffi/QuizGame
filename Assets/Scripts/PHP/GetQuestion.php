0ca87003418e501e850ef685f22db3076dbce28b
Pa0ran <niklas.malinen@metropolia.fi>
2020-12-01 14:26:57 +0200

    Kommentoitu GetQuestion

--- Assets/Scripts/PHP/GetQuestion.php
+++ Assets/Scripts/PHP/GetQuestion.php
@@ -1,34 +1,29 @@
 ﻿<?php
      
+     // hyväksyy HTTP:n eikä vaadi HTTPS
      header('Access-Control-Allow-Origin: *');
+     // Kirjaimet oikeaan muotooon
      header('Content-type: text/html; charset=ISO-8859-1');
+     // kirjautumistiedot
      $hostname = 'mysql.metropolia.fi';
      $username = 'joonaaal';
      $password = 't13t0k1lp4';
      $database = 'joonaaal';
      
      
-     /*if(isset($_REQUEST["var1"]))
-     {
-         echo "received ". $_REQUEST["var1"]. " success!";
-         
-     }
-     else
-     {
-         echo "Request Failed";
-     }*/
      
- 
+     
+     // yhdistetään tietokantaan
      $con = mysqli_connect($hostname, $username, $password, $database);
- 
+    //tarkistetaan yhdistyykö tietokantaan
      if(mysqli_connect_errno()){
  
         echo "Failed to connect " . mysqli_connect_errno();
      }
- 
+     // lähetetään tietokannalle kysely
      $result = mysqli_query($con, $_REQUEST["var1"]);
      echo ";";
- 
+     // käydään läpi se mitä haettu tietokannassa
      while ($row = mysqli_fetch_array($result)) {
        
         echo $row['kysymys']."_". $row['vaihtoehto1']."_". $row['vaihtoehto2'] ."_". $row['vaihtoehto3'] ."_". $row['vaihtoehto4']. "_".
